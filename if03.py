a12cee7aa3f7e648fa87e7300febd64b38393e0e
Helengendary <helenapicinin13@gmail.com>
2024-09-13 16:59:29 -0300

    Exercicio 14

---
+++ if03.py
@@ -0,0 +1,3 @@
+# https://github.com/Andredev-dias/Base-de-Algoritmos-em-C/blob/master/if03.c
+
+print("Este é um número par.\n" if (float(input('INFORME UM NÚMERO: '))%2)==0 else "Este é um número ímpar.\n")
