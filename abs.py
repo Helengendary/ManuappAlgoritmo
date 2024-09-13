b26c5ee12e83e4e7eb675780761765a0b12a08a0
Helengendary <helenapicinin13@gmail.com>
2024-09-13 16:53:09 -0300

    Exercicio 12

---
+++ abs.py
@@ -0,0 +1,9 @@
+# https://github.com/Andredev-dias/Base-de-Algoritmos-em-C/blob/master/abs.c
+
+
+x = float(input(" Qual o primeiro numero \n"))
+
+y = float(input(" Qual o segundo numero \n"))
+
+resp = abs(x-y)
+print("A diferença entre {x} e {y} é: {resp}\n")
