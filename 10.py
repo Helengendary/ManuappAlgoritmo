16b0f86aff4bc76371e52ca953c24e6b802cd2fa
Helengendary <helenapicinin13@gmail.com>
2024-09-13 16:50:06 -0300

    Exercicio 11

---
+++ 10.py
@@ -0,0 +1,29 @@
+# https://github.com/Andredev-dias/Base-de-Algoritmos-em-C/blob/master/10.c
+
+
+class competidor:
+    def __init__(self, codigo, age, height, weight) :
+        self.codigo = codigo
+        self.age = age
+        self.height = height
+        self.weight = weight
+        
+    def __repr__(self):
+     return str(self.__dict__)
+
+
+codigo = int(input("\nInforme o codigo:"))
+
+age = int(input("\nInforme a idade:"))
+
+altura = float(input("\nInforme a altura:"))
+
+peso = float(input("\nInforme o peso:"))
+
+comp = competidor(codigo, age, altura, peso)
+
+print("\n****************************************************")
+print("****************************************************")
+print("****************************************************\n")
+
+printf(f"O competidor {comp.codigo} , tem {comp.age} anos, pesa {comp.weight} Kg e tem {comp.height} de altura. \n")
